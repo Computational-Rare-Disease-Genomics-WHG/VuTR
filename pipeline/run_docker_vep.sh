3b6faf5a8d15a71c87635be3794cf8ea276a4642
elston <elstonndsouza@gmail.com>
2021-08-05 16:31:21 +1000

    Docker + VEP + UTR Annotator finally works

--- pipeline/run_docker_vep.sh
+++ pipeline/run_docker_vep.sh
@@ -2,16 +2,17 @@
 
 # To run an interactive terminal inside docker 
 docker run -t -i \
- -v $(pwd)/UTRannotator/:/opt/vep/.vep/Plugins \
- -v $(pwd)/input_vep:/input_vep \
- -v $(pwd)/output_vep:/output_vep \
+ -v $(pwd)/vep_data:/opt/vep/.vep\
+ -v /home/elston/Packages/vep_cache:/opt/vep/.vep/Cache\
  ensemblorg/ensembl-vep \
- ./vep -i /input_vep/test_grch38.vcf \
+ ./vep \
  --assembly GRCh38\
  --force_overwrite\
  --species homo_sapiens\
+ --cache \
+ --offline\
  --tab \
- --database \
- -plugin UTRannotator,\
- ./UTRannotator/uORF_5UTR_GRCh38_PUBLIC.txt\
- -o /output_vep/testrun_vep.test_grch38.out
+ --dir_cache /opt/vep/.vep/Cache \
+ --plugin UTRannotator, /opt/vep/.vep/Plugins/uORF_starts_ends_GRCh38_PUBLIC.txt\
+ -i /opt/vep/.vep/input/test_grch38.vcf \
+ -o /opt/vep/.vep/output/testrun_vep.test_grch38.out
