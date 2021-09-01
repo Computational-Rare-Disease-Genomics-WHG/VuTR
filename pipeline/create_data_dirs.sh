c716e3ae472e1da055b48c4d4cef7b5a10f5388e
elston <elstonndsouza@gmail.com>
2021-09-01 12:14:34 +1000

    Remove VEP outputs and only store UTR annotator source code in repo

--- pipeline/create_data_dirs.sh
+++ pipeline/create_data_dirs.sh
@@ -8,3 +8,8 @@ mkdir -p data/pipeline/vep_data
 
 
 # Create the vep_data pipeline
+mkdir -p data/pipeline/vep_data/input
+mkdir -p data/pipeline/vep_data/output
+mkdir -p data/pipeline/vep_data/Plugins
+
+# Copy UTR Annotator source code to Plugins
