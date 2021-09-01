eb31bb3a2ec2fef6130cc93987ded17a80c095d3
elston <elstonndsouza@gmail.com>
2021-09-01 12:24:24 +1000

    Update create_data_dirs.sh for other clinvar / mane / sorfs / gnomad

--- pipeline/create_data_dirs.sh
+++ pipeline/create_data_dirs.sh
@@ -14,3 +14,10 @@ mkdir -p data/pipeline/vep_data/Plugins
 
 # Copy UTR Annotator source code to Plugins
 cp -R -u -p pipeline/UTR-Annotator/* data/pipeline/vep_data/Plugins
+
+
+# Create mane / sorfs / gnomad / clinvar data directories
+mkdir -p data/pipeline/MANE
+mkdir -p data/pipeline/SORFS
+mkdir -p data/pipeline/CLINVAR
+mkdir -p data/pipeline/GNOMAD
