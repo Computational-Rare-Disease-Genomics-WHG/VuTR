e8c545a74d213b1b4926e885101eac2b635f37fc
elston <elstonndsouza@gmail.com>
2021-09-01 15:54:17 +1000

    Update clinvar script and add logging to downloading sorfs

--- pipeline/downloads/download_clinvar.sh
+++ pipeline/downloads/download_clinvar.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# Download the ClinVar's weekly release
+cd $(dirname "$0")
 
+# Download the ClinVar's weekly release
 curl "ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/weekly/clinvar.vcf.gz" \
-    --output vep_data/input/clinvar.vcf.gz
+    --output data/pipeline/CLINVAR/
