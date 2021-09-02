78670fbfcbdacf123e1812d044a337134a41fe91
elston <elstonndsouza@gmail.com>
2021-09-02 17:26:38 +1000

    Update viewer with new directories and add ClinGen and LOEUF to viewer modules

---
+++ server/flask-app/app/tools/clingen.py
@@ -0,0 +1,23 @@
+"""
+Provides access to ClinGen data
+"""
+import pandas as pd
+
+
+def get_clingen_curation(hgnc):
+    """
+    Searches through clingen for the haploinsufficency score
+
+    @param hgnc (str) : HGNC Gene Symbol
+    @returns clingen_curation_record (dict) : The dictionary\
+         associated with the clingen record
+    """
+    clingen_curation = pd.read_csv(
+        '../../data/pipeline/CLINGEN/ClinGen_gene_curation_list_GRCh38.tsv',
+        sep='\t',
+        skiprows=5,
+    )
+    clingen_curation_record = clingen_curation[
+        clingen_curation['#Gene Symbol'] == hgnc
+    ].to_dict('records')[0]
+    return clingen_curation_record
