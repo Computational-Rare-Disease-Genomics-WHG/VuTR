c6215f0dd8daf0d510ac17cbebde013624cecff0
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-09-13 19:22:01 +1000

    Change default transcript

--- server/flask-app/app/main.py
+++ server/flask-app/app/main.py
@@ -1,39 +1,46 @@
-# main.py 
-# E. D'Souza 
+# main.py
+# E. D'Souza
 
 from flask import (
-    Blueprint, 
-    render_template, 
-    url_for, 
+    Blueprint,
+    render_template,
+    url_for,
     redirect)
 
 import itertools
-import datetime 
+import datetime
 
 main = Blueprint('main', __name__)
 
-# Home Page 
+# Home Page
+
+
 @main.route("/")
-def index(): 
-    #return render_template("index.html")
-    return redirect(url_for("viewer.viewer_page", ensembl_transcript_id="ENST00000340208"))
+def index():
+    # return render_template("index.html")
+    return redirect(url_for("viewer.viewer_page", ensembl_transcript_id="ENST00000504921"))
+
 
 @main.route("/about")
-def about(): 
+def about():
     return render_template("about.html")
 
+
 @main.route("/help")
-def help(): 
+def help():
     return render_template("help.html")
 
+
 @main.route("/contact")
-def contact(): 
+def contact():
     return render_template("contact.html")
 
+
 @main.route("/change_log")
-def change(): 
+def change():
     return render_template("change_log.html")
 
+
 @main.route("/download")
-def download(): 
+def download():
     return render_template("")
