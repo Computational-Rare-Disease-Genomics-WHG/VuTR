4a33e8d8751115ea9205453b38df2edc31a2bbbc
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-09-13 19:23:53 +1000

    created a wrapper function to call utility transcript features

--- server/flask-app/app/tools/mane.py
+++ server/flask-app/app/tools/mane.py
@@ -3,9 +3,10 @@ Provides utility function to get access to the feature track data from MANE
 """
 
 import pandas as pd
+from .utils import find_uorfs_in_transcript, convert_betweeen_identifiers
 
 
-def read_mane(ensembl_gene_id):
+def read_mane_genomic_features(ensembl_gene_id):
     """
     Reads mane for a specific gene_id from the genomic feature file
 
@@ -19,8 +20,54 @@ def read_mane(ensembl_gene_id):
         sep='\t',
     )
     mane['ensembl_stable_gene_id'] = mane['gene_id'].apply(lambda x: str(x)[0:15])
-    gene_data = mane[mane['ensembl_stable_gene_id'] == ensembl_gene_id]
+    gene_data = mane[mane['gene_id'] == ensembl_gene_id]
     return gene_data
+
+
+def read_mane_transcript(ensembl_transcript_id):
+    """
+    Reads through the MANE transcript set to get the transcript sequences
+
+    @param ensembl_transcript_id (str) : Full transcript, id (including version number)
+    @returns transcript_df
+    """
+
+    transcript_df = pd.read_csv(
+        "../../data/pipeline/MANE/0.93/MANE_transcripts_v0.93.tsv", sep="\t")
+    transcript_df = transcript_df[transcript_df["ensembl_transcript_id"].str.contains(
+        ensembl_transcript_id)]
+
+    return transcript_df
+
+
+def get_transcript_features(ensembl_transcript_id):
+
+    # read through the transcript sequence
+    transcript_feats = {}
+
+    transcript_entry = read_mane_transcript(ensembl_transcript_id=ensembl_transcript_id)
+
+    # get the start and  end points of the transcript.
+    gene_id = convert_betweeen_identifiers(
+        ensembl_transcript_id, "ensembl_transcript", "ensembl_gene")
+    utr_stats = get_utr_stats(gene_id)
+
+    transcript_feats["full_seq"] = transcript_entry["seq"].values[0]
+
+    # find the start sites
+    transcript_feats["start_site"] = utr_stats["5_prime_utr_length"]
+
+    # find the stop sites
+
+    # find all uORFs
+    transcript_feats["uORF"] = find_uorfs_in_transcript(
+        seq=transcript_feats["full_seq"],
+        start_site=transcript_feats["start_site"])
+
+    # find oORFS
+    #transcript_feats["oORFs"] = find_oorf_in_transcript()
+
+    return transcript_feats
 
 
 def get_utr_stats(ensembl_gene_id):
@@ -33,7 +80,7 @@ def get_utr_stats(ensembl_gene_id):
     @returns utr_stats (dict) : Statistics of the five prime utr
 
     """
-    gene_data = read_mane(ensembl_gene_id)
+    gene_data = read_mane_genomic_features(ensembl_gene_id)
     five_prim_utrs = gene_data[gene_data['type'] == 'five_prime_UTR']
     five_prim_utrs['width'] = five_prim_utrs['end'] - five_prim_utrs['start'] + 1
     utr_stats = {}
@@ -53,7 +100,7 @@ def get_gene_features(ensembl_gene_id):
     @returns: gene_features (dict) : A dictionary for the
      genomic features of the specified ensembl_gene_id.
     """
-    gene = read_mane(ensembl_gene_id)
+    gene = read_mane_genomic_features(ensembl_gene_id)
     gene_features = gene[gene['type'] == 'gene'].to_dict('records')[0]
     return gene_features
 
@@ -71,7 +118,7 @@ def genomic_features_by_ensg(ensembl_gene_id):
     """
 
     # Load up MANE
-    gene_data = read_mane(ensembl_gene_id)
+    gene_data = read_mane_genomic_features(ensembl_gene_id)
     gene_data['width'] = gene_data['end'] - gene_data['start'] + 1
     genomic_features = {}
     genomic_features['gene_start'] = gene_data[gene_data['type'] == 'gene'][
