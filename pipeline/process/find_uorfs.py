521b11f97b8dbda52832e765a8ec147c83741c32
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-09-30 15:18:26 +0100

    fix

--- pipeline/process/find_uorfs.py
+++ pipeline/process/find_uorfs.py
@@ -1,27 +1,15 @@
 """ Finds the uORFs within a sequence"""
 
-import pandas as pd
-from utr_utils.tools.mane import read_mane_transcript
-from utr_utils.tools.mane import find_uorfs_in_transcript
+from utr_utils.tools.mane import get_transcript_features
 
 write_location = "../../data/pipeline/UORFS.tsv"
 
 
 def main():
     # Read in mane
-
-    summary =  # read summary mane
-
-    uorfs_df = pd.DataFrame()
-    for key, row in summary.iterrows():
-        print(f'Finding uORFS in {row["Ensembl_nuc"]}')
-        transcript = read_mane_transcript(row['Ensembl_nuc'])
-        uorfs = find_uorfs_in_transcript(transcript)
-        append_row = pd.DataFrame(uorfs)
-        uorfs_df = pd.concat([uorfs_df, append_row])
-
-    # Write file
-    uorfs_df.to_csv(write_location, sep="\t")
+    transcript_id = "ENST00000504921.7"
+    transcript_feats = get_transcript_features(transcript_id)
+    print(transcript_feats["uORF"])
 
 
 if __name__ == '__main__':
