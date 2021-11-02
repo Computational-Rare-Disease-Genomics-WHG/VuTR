0812ed3a840fcd8a32e5143e036ead4219b9a107
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-11-02 08:45:27 +0000

    Split out running generating possible variants to chrom specific

--- pipeline/process/generate_all_possible_variants.py
+++ pipeline/process/generate_all_possible_variants.py
@@ -1,4 +1,4 @@
-"""Creates a TSV file that creates all possible UTR variants for VEP. 
+"""Creates a TSV file that creates all possible UTR variants for VEP.
 TODO : Add indels as well
 """
 
@@ -49,58 +49,64 @@ def main(args):
     features['width'] = features['end'] - features['start']+1
 
     # Write output
-    if args.only_chr_22:
-        # filter utr_file to chr_22
-        features = features[features['seqid'] == 'chr22']
 
     features = features.sort_values(by=['gene_id', 'exon_number'])
     chrom_possible_df = pd.DataFrame()
-    for gene in features['gene_id'].unique():
+    if args.only_chr_22:
+        # filter utr_file to chr_22
+        chroms = ['22']
 
-        # find all of utr features  within that region
-        feats = features[features['gene_id'] == gene]
-        chrom = feats['seqid'].values[0]
-        strand = feats['strand'].values[0]
-        utr_length = sum(feats['width'])
+    formated_chroms = ["chr"+i for i in chroms]
 
-        # find the sequence
-        seqs = list(
-            transcript_sequences[transcript_sequences['gene'] == gene].seq.values[0][0:utr_length].upper())
+    for chrom in formated_chroms:
 
-        # Get the list of positions
-        pos = list(
-            chain(*list(feats.apply(lambda x: list(range(x["start"], x["end"]+1)), axis=1))))
+        for gene in features[features['seqid'] == chrom]['gene_id'].unique():
 
-        if strand == '-':
-            # sort based on strand
-            pos = sorted(pos, reverse=True)
+            # find all of utr features  within that region
+            feats = features[features['gene_id'] == gene]
+            chrom = feats['seqid'].values[0]
+            strand = feats['strand'].values[0]
+            utr_length = sum(feats['width'])
 
-            # get complement of the sequence
-            seqs = [complement_bases[nt] for nt in seqs]
+            # find the sequence
+            seqs = list(
+                transcript_sequences[transcript_sequences['gene'] == gene].seq.values[0][0:utr_length].upper())
 
-        # Simulate a dataframe of all variants
-        long_df = pd.DataFrame({'chrom': [chrom[3:]]*(utr_length*4),
-                                'start': pos*4,
-                                'end': pos*4,
-                                'ref': seqs*4,
-                                'alt': bases * utr_length,
-                                'strand': [strand] * (utr_length*4)})
+            # Get the list of positions
+            pos = list(
+                chain(*list(feats.apply(lambda x: list(range(x["start"], x["end"]+1)), axis=1))))
 
-        # Remove all rows with ref same as alt
-        long_df = long_df[long_df['ref'] != long_df['alt']]
+            if strand == '-':
+                # sort based on strand
+                pos = sorted(pos, reverse=True)
 
-        # Format to VEP input
-        long_df['allele'] = long_df['ref'] + "/" + long_df['alt']
-        long_df = long_df.loc[:, ['chrom', 'start', 'end', 'allele', 'strand']]
-        long_df = long_df.reset_index(drop=True)
-        chrom_possible_df = pd.concat([chrom_possible_df, long_df])
+                # get complement of the sequence
+                seqs = [complement_bases[nt] for nt in seqs]
 
-    vprint(f'Writing to VEP file', args.verbose)
-    chrom_possible_df.to_csv(script_path /
-                             f"../../data/pipeline/vep_data/input/UTR_variants_all_possible_{assembly}_{mane_version}.txt",
-                             sep="\t",
-                             header=None,
-                             index=False)
+            # Simulate a dataframe of all variants
+            long_df = pd.DataFrame({'chrom': [chrom[3:]]*(utr_length*4),
+                                    'start': pos*4,
+                                    'end': pos*4,
+                                    'ref': seqs*4,
+                                    'alt': bases * utr_length,
+                                    'strand': [strand] * (utr_length*4)})
+
+            # Remove all rows with ref same as alt
+            long_df = long_df[long_df['ref'] != long_df['alt']]
+
+            # Format to VEP input
+            long_df['allele'] = long_df['ref'] + "/" + long_df['alt']
+            long_df = long_df.loc[:, ['chrom', 'start', 'end', 'allele', 'strand']]
+            long_df = long_df.reset_index(drop=True)
+            chrom_possible_df = pd.concat([chrom_possible_df, long_df])
+
+        vprint(f'Finish generating mutations for {chrom}')
+        vprint(f'Writing to VEP file', args.verbose)
+        chrom_possible_df.to_csv(script_path /
+                                 f"../../data/pipeline/vep_data/input/UTR_variants_all_possible_{assembly}_{mane_version}_{chrom}.txt",
+                                 sep="\t",
+                                 header=None,
+                                 index=False)
 
 
 if __name__ == '__main__':
