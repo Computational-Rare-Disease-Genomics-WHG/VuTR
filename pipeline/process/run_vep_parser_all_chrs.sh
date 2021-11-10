ec068fc5c40808bd7b2c29b3746138231b0f921b
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-11-10 14:26:25 +0000

    Run utr annotator parsers for all chrs

---
+++ pipeline/process/run_vep_parser_all_chrs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Flag : To be replaced by worflow management
+# Runs the parser on all possible variants
+
+INPATH='../../data/pipeline/vep_data/output'
+OUTPATH='../../data/pipeline/vep_data/output'
+VEP_CACHE='/well/whiffin/shared/vep'
+ASSEMBLY='GRCh38'
+MANE_VERSION='0.93'
+HEADER_LINES=47
+
+for i in {1..22} X Y
+do
+    echo "Running parser on chr ${i}"
+    python3 parse_vep_outputs.py  \
+    --vep_file ${INPATH}/UTR_variants_vep_all_possible_GRCh38_0.93_chr${i}.txt \
+    --output_file ${OUTPUT}/UTR_variants_all_possible_GRCh38_0.93_chr_${i}_parsed_vep.txt \
+    --header_lines $HEADER_LINES
+done
