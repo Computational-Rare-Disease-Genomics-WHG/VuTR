8d0058bfa3a0a8cddfed3e67296605d601eccbe0
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-11-11 10:57:24 +0000

    Run Tabix

--- pipeline/process/run_tabix_gnomAD.sh
+++ pipeline/process/run_tabix_gnomAD.sh
@@ -12,7 +12,7 @@ for i in {1..22} X Y
 do
     echo "Filtering gnomAD on chr${i}"
 
-    
+    # Tabix
     tabix -p vcf {GNOMAD_INPATH}/gnomad.genomes.v3.1.1.sites.chr${i}.vcf.bgz -f
 
     # Filter gnomAD to sites
