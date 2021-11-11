da24577492e67a5e9353df4acfa0396207fc4504
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-11-11 10:39:27 +0000

    Test out tabix

--- pipeline/process/run_tabix_gnomAD.sh
+++ pipeline/process/run_tabix_gnomAD.sh
@@ -10,10 +10,12 @@ GNOMAD_OUTPUT='data/pipeline/GNOMAD'
 # Iterate through the chroms
 for i in {1..22} X Y
 do
-    echo "Filtering gnomAD on ${i}"
+    echo "Filtering gnomAD on chr${i}"
     
     # Filter gnomAD to sites
     tabix ${GNOMAD_INPATH}/gnomad.genomes.v3.1.1.sites.chr${i}.vcf.bgz \
-            -R data/pipeline/UTR_regions.tsv \
+            --regions data/pipeline/UTR_regions.tsv \
+            --preset vcf \
+            --force \
             > ${GNOMAD_OUTPUT}/gnomad.genomes.v3.1.1.utr_sites.chr${i}.vcf
 done
