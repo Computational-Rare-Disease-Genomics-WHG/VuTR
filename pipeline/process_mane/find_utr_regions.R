440a5610ea19a4844a01dbd81fb4fd4d9cdf0dfb
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-11-11 20:16:39 +0000

    rename file

---
+++ pipeline/process_mane/find_utr_regions.R
@@ -0,0 +1,28 @@
+# find_utr_regions.R
+# E. D'Souza
+
+# Takes the MANE genomic features file and then
+# creates a regions file for tabix to run with the -R
+# flag
+
+library(data.table)
+library(magrittr)
+library(stringr)
+library(stringi)
+
+setwd("../..")
+
+mane_features <- "data/pipeline/MANE/0.93/MANE.GRCh38.v0.93.select_ensembl_genomic.tsv" %>% # nolint
+    fread(., sep = "\t")
+
+mane_features %<>% .[type == "five_prime_UTR"]
+mane_features %<>% .[, .(seqid, start, end)]
+names(mane_features) <- c("CHROM", "POS", "POS_TO")
+mane_features[, CHROM := as.character(CHROM)]
+mane_features[, CHROM := substr(CHROM, 4, nchar(CHROM))]
+
+fwrite(mane_features,
+    "data/pipeline/UTR_regions.tsv",
+    sep = "\t",
+    col.names = F
+)
