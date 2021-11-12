b8bfa033c04a2006e588aa35792329131ef2c38b
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-11-12 12:53:51 +0000

    Update file names

--- pipeline/variants/run_vep_parser_all_chrs.sh
+++ pipeline/variants/run_vep_parser_all_chrs.sh
@@ -5,6 +5,7 @@
 
 INPATH='../../data/pipeline/vep_data/output'
 OUTPATH='../../data/pipeline/vep_data/output'
+PIPLINE_OUTPATH='../../data/pipeline'
 VEP_CACHE='/well/whiffin/shared/vep'
 ASSEMBLY='GRCh38'
 MANE_VERSION='0.93'
@@ -15,6 +16,12 @@ do
     echo "Running parser on chr ${i}"
     python3 parse_vep_outputs.py  \
     --vep_file ${INPATH}/UTR_variants_vep_all_possible_GRCh38_0.93_chr${i}.txt \
-    --output_file ${OUTPATH}/UTR_variants_all_possible_GRCh38_0.93_chr_${i}_parsed_vep.txt \
+    --output_file ${OUTPATH}/UTR_variants_all_possible_GRCh38_0.93_chr${i}_parsed_vep.txt \
     --header_lines $HEADER_LINES
 done
+
+# Merge all of the variants into a single file
+head -n 1 ${OUTPATH}/UTR_variants_all_possible_GRCh38_0.93_chr1_parsed_vep.txt > \
+ ${PIPLINE_OUTPATH}/UTR_variants_all_possible_GRCh38_0.93_all_chrs.tsv && \
+ tail -n+2 -q ${OUTPATH}/UTR_variants_all_possible_GRCh38_0.93_chr*_parsed_vep.txt >> \
+  ${PIPLINE_OUTPATH}/UTR_variants_all_possible_GRCh38_0.93_all_chrs.tsv
