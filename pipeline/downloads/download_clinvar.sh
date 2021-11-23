34de3796d232e8a66f7861a5c8426f9712eaae72
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-11-23 13:14:50 +0000

    Update new download scripts

--- pipeline/downloads/download_clinvar.sh
+++ pipeline/downloads/download_clinvar.sh
@@ -5,5 +5,6 @@ cd $(dirname "$0")
 cd ../../
 
 # Download the ClinVar's weekly release
-curl "ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/weekly/clinvar.vcf.gz" \
+curl \
+"ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/vcf_GRCh38/weekly/clinvar.vcf.gz" \
     --output data/pipeline/CLINVAR/clinvar.vcf.gz
