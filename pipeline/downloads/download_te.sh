a98ae9e7dd282a508a6f54cbeef4cf4bd6669e96
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-12-02 17:22:43 +0000

    Update TE file to fix headers and changing the rna sequence to dna

--- pipeline/downloads/download_te.sh
+++ pipeline/downloads/download_te.sh
@@ -5,4 +5,14 @@
 # Noderer, William L., et al. "Quantitative analysis of mammalian translation initiation sites by FACS‐seq." Molecular systems biology 10.8 (2014): 748.
 
 
-curl "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4299517/bin/msb0010-0748-SD3.txt" | sed 1d  > ../../data/pipeline/translational_efficiency.txt
+curl "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4299517/bin/msb0010-0748-SD3.txt" |\
+ sed 1d |\
+ tr [:upper:] [:lower:] |\
+ sed "1d;s/u/t/g" > tmp.txt
+
+echo $'context\tefficiency\tlower_bound\tupper_bound'|
+ cat -  tmp.txt |\
+ sed "s/^M/\r/g" >\
+ ../../data/pipeline/translational_efficiency.txt
+
+rm tmp.txt
