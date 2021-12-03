0d687f134fea544b47b1a4745f6ebfa57cefe020
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-12-03 12:27:57 +0000

    Comments

--- pipeline/downloads/download_te.sh
+++ pipeline/downloads/download_te.sh
@@ -4,12 +4,15 @@
 # Download the translational efficiency from
 # Noderer, William L., et al. "Quantitative analysis of mammalian translation initiation sites by FACS‐seq." Molecular systems biology 10.8 (2014): 748.
 
-
+# Download into tmp
+# Change uppercase sequences to lowercase
+# and replace uracils to thymines
 curl "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4299517/bin/msb0010-0748-SD3.txt" |\
  sed 1d |\
  tr [:upper:] [:lower:] |\
  sed "1d;s/u/t/g" > tmp.txt
 
+# Add headers and remove windows-style line endings
 echo $'context\tefficiency\tlower_bound\tupper_bound'|
  cat -  tmp.txt |\
  sed "s/^M/\r/g" >\
