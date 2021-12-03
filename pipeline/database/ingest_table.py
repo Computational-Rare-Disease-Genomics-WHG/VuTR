41d9a522572fac4d933ba14347ef36da0bf93486
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-12-03 15:09:05 +0000

    Add docstrings, working ingestion engines

--- pipeline/database/ingest_table.py
+++ pipeline/database/ingest_table.py
@@ -1,12 +1,57 @@
-"""Takes a table and ingests it into the sqlite3 database"""
+"""
+Takes a table and ingests it into the sqlite3 database
 
-
+Usage : python3 ingest_table.py --db_name db.sqlite --all --verbose --overwrite
+"""
+from pathlib import Path
 import sqlite3
 import argparse
 import sys
 import os
+import sqlalchemy
+
+from model import tbl_models  # pylint: disable=E0401
 import pandas as pd
-from .model import tbl_models
+
+SCRIPT_PATH = Path(__file__).parent
+
+
+def sqlcol(dfparam):
+    """
+    Create a dictionary for native SQL alchemy types
+    from a dataframe column
+
+    Note : Doesn't work for string for some reason
+    hence not used in main()
+    """
+
+    dtypedict = {}
+    for i, j in zip(dfparam.columns, dfparam.dtypes):
+        if 'object' in str(j):
+            dtypedict.update({i: sqlalchemy.types.NVARCHAR()})
+
+        if 'datetime' in str(j):
+            dtypedict.update({i: sqlalchemy.types.DateTime()})
+
+        if 'float' in str(j):
+            dtypedict.update({i: sqlalchemy.types.Float(precision=3, asdecimal=True)})
+
+        if 'int' in str(j):
+            dtypedict.update({i: sqlalchemy.types.INT()})
+
+    return dtypedict
+
+
+def strip_version_identifiers(df, id_names):
+    """
+    Use grep to remove trailing ENSTXXX[.3] - >  ENSTXXX
+    @param df
+    """
+
+    df.loc[:, id_names] = df.loc[:, id_names].replace(
+        to_replace='\.\d+$', value='', regex=True  # pylint: disable=W1401 # noqa: W605
+    )
+    return df
 
 
 def main(args):
@@ -30,18 +75,30 @@ def main(args):
     # read the database
     for tbl in tbl_list:
 
+        # Check if the keys in
+
         # Read the sql table
-        df = pd.read_csv('')
+        print(f'Reading table {tbl} into pandas')
+        df = pd.read_csv(
+            SCRIPT_PATH / ('../../data/pipeline' + tbl_models[tbl]['location']),
+            sep=tbl_models[tbl]['separator'],
+        )
+
+        # Select columns specified
+        df = df.loc[:, tbl_models[tbl]['col_mappings'].keys()]
+        # Replace column names with the col_mappings
+        df = df.rename(columns=tbl_models[tbl]['col_mappings'])
+
+        # Strip ensembl_identifiers for the columns
+        if tbl_models[tbl]['remove_ensembl_id_version_numbers']:
+            df = strip_version_identifiers(df, tbl_models[tbl]['ensembl_ids'])
 
         # Write to SQLtables
-        df.to_sql(
-            tbl,
-            conn,
-            if_exists='fail',
-            index=False,
-            chunksize=1000,
-            dtype=tbl_models[tbl],
-        )
+
+        # TODO : Need to figure out of a way
+        print(f'Writing to SQLite DB {args.db_name}')
+        df.to_sql(tbl, conn, if_exists='fail', index=False, chunksize=1000)
+        print(f'Ingestion for table {tbl} complete')
 
 
 if __name__ == '__main__':
@@ -54,10 +111,20 @@ if __name__ == '__main__':
         help='Output sqlite3 file name and location',
     )
     parser.add_argument(
-        '--variant_file',
-        required=True,
+        '--single',
+        action='store_true',
+        help='Single table',
+    )
+    parser.add_argument(
+        '--all',
+        action='store_true',
+        help='All tables',
+    )
+    parser.add_argument(
+        '--tbl_name',
+        required='single' in sys.argv,
         type=str,
-        help='The variant tsv file to be injested',
+        help='The name of the table to be ingested',
     )
     parser.add_argument(
         '--overwrite',
