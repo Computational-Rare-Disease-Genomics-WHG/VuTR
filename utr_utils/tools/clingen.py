b52e5cd5a542d59dbbf7a853da6a4df0d41cae5e
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-12-06 15:58:46 +0000

    Fix clingen bug

--- utr_utils/tools/clingen.py
+++ utr_utils/tools/clingen.py
@@ -1,8 +1,9 @@
 """
 Provides access to ClinGen data
 """
-import pandas as pd
+
 from pathlib import Path
+import pandas as pd
 
 script_path = Path(__file__).parent
 
@@ -15,11 +16,11 @@ def get_clingen_curation(hgnc):
     @returns clingen_curation_record (dict) : The dictionary
          associated with the clingen record
     """
-    clingen_curation = pd.read_csv(script_path /
-                                   '../../data/pipeline/CLINGEN/ClinGen_gene_curation_list_GRCh38.tsv',
-                                   sep='\t',
-                                   skiprows=5,
-                                   )
+    clingen_curation = pd.read_csv(
+        script_path
+        / '../../data/pipeline/CLINGEN/ClinGen_gene_curation_list_GRCh38.tsv',
+        sep='\t',
+    )
     if hgnc in clingen_curation['#Gene Symbol']:
         clingen_curation_record = clingen_curation[
             clingen_curation['#Gene Symbol'] == hgnc
