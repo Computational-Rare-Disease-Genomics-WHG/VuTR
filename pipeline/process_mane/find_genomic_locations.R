598b0856d9f46b43797b65a3f88314352862afdd
ENDNeilD'souza <elstonndsouza@gmail.com>
2021-12-07 14:59:24 +0000

    Add script to look up and add the genomic positions of uORFS / oORFs

--- pipeline/process_mane/find_genomic_locations.R
+++ pipeline/process_mane/find_genomic_locations.R
@@ -1,3 +1,106 @@
-# find_genomic_locations.R
+library(data.table)
+library(magrittr)
 
-# Find the genomic locations of uORFS
+setwd("../../")
+uorfs <- fread("data/pipeline/ORFS_Features_0.93.tsv")
+mane_gff <- "data/pipeline/MANE/0.93/MANE.GRCh38.v0.93.select_ensembl_genomic.tsv"  # nolint
+    %>% fread() # nolint
+
+# Filter to exons only
+mane_gff %<>% .[type == "exon"]
+
+# set order by the transcript id
+setorder(mane_gff, "transcript_id", "exon_number")
+
+# Create the width of the exon
+mane_gff[, exon_length := end - start + 1]
+
+# Create the relative transcript start and end widths
+mane_gff[, transcript_end := cumsum(exon_length), by = transcript_id]
+mane_gff[, transcript_start := cumsum(data.table::shift(exon_length,
+    fill = 1
+)), by = transcript_id]
+
+# Get the start position by looking up mane_gff
+setkey(mane_gff, transcript_id, transcript_start, transcript_end)
+
+uorfs_genomic_positions <- data.table()
+
+for (i in seq(nrow(uorfs))) {
+    print(i)
+    # Find values for this iteration
+    orf_id <- uorfs[i]$orf_id
+    this_transcript_id <- uorfs[i]$ensembl_transcript_id
+    orf_start <- uorfs[i]$orf_start_codon
+    orf_end <- uorfs[i]$orf_stop_codon
+
+    # Filter the gff entries and strand corresponding to this transcript
+    transcript_gff <- mane_gff[transcript_id == this_transcript_id]
+    strand <- transcript_gff[1]$strand
+
+    if (strand == "+") {
+        # find the genomic positions for the ATG
+        # and the start of all succeeding exons
+
+        genomic_starts <- c(
+            transcript_gff[(transcript_start <= orf_start) &
+                (transcript_end >= orf_start)]$start +
+
+                (orf_start - transcript_gff[(transcript_start <= orf_start) &
+                    (transcript_end >= orf_start)]$transcript_start), # nolint
+
+            # Find the genomic start positions
+            # of all of the exons until the exon where the stop is located
+            transcript_gff[transcript_start >= orf_start &
+                transcript_end <= orf_end]$start
+        )
+
+        # find the genomic location of the end of all of the exons
+        genomic_ends <- c(
+            transcript_gff[(transcript_start >= orf_start) &
+                (transcript_end <= orf_end)]$end,
+
+            # Find the genomic position of the TGA/TAA/TAG
+            transcript_gff[(transcript_start <= orf_end) &
+                (transcript_end >= orf_end)]$start +
+                (orf_end - transcript_gff[(transcript_start <= orf_end) &
+                    (transcript_end >= orf_end)]$transcript_start)
+        )
+    } else {
+        # For the reverse strand
+
+        # Find the genomic position of the stop codon
+        genomic_starts <- c(
+            transcript_gff[(transcript_start <= orf_end) &
+                (transcript_end >= orf_end)]$start +
+                (orf_end - transcript_gff[(transcript_start <= orf_end) &
+                    (transcript_end >= orf_end)]$transcript_end), # nolint
+            transcript_gff[transcript_end <= orf_end &
+                transcript_start >= orf_start]$start
+        )
+
+        # Find the genomic position of the ATG start codon
+        genomic_ends <- c(
+            transcript_gff[transcript_end <= orf_end &
+                transcript_start >= orf_start]$end,
+            transcript_gff[(transcript_start <= orf_start) &
+                (transcript_end >= orf_start)]$start +
+                (orf_start - transcript_gff[(transcript_start <= orf_start) &
+                    (transcript_end >= orf_start)]$transcript_end)
+        )
+    }
+    # Create a dataframe to row bind this to
+    n <- length(genomic_ends)
+    uorfs_genomic_positions <- rbind(
+        uorfs_genomic_positions,
+        data.table(
+            orf_id = rep(orf_id, n),
+            strand = rep(strand, n),
+            transcript_id = rep(this_transcript_id, n),
+            start = genomic_starts,
+            end = genomic_ends
+        )
+    )
+}
+
+fwrite(uorfs_genomic_positions, "data/pipeline/UORFS_Genomic_Positions.tsv")
