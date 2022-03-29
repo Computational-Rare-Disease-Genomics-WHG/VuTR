bb9bc8b102c19361390cb8749890e65505e6123d
ENDNeilD'souza <elstonndsouza@gmail.com>
2022-03-29 09:20:23 +0400

    Add not found page

--- server/flask-app/app/main.py
+++ server/flask-app/app/main.py
@@ -16,13 +16,22 @@ main = Blueprint('main', __name__)
 @main.route('/gene_search', methods=['POST'])
 def gene_search():
     """Search by gene"""
-    gene_name = request.form['gene_q']
+    gene_name = request.form['gene_q'].upper().strip()
+
     ensembl_transcript_id = convert_between_ids(
         gene_name, 'hgnc_symbol', 'ensembl_transcript_id'
     )
-    return redirect(
-        url_for('viewer.viewer_page', ensembl_transcript_id=ensembl_transcript_id)
-    )
+    if ensembl_transcript_id is not None:
+        return redirect(
+            url_for('viewer.viewer_page', ensembl_transcript_id=ensembl_transcript_id)
+        )
+    return redirect(url_for('main.not_found'))
+
+
+@main.route('/gene_not_found')
+def not_found():
+    """Not found page"""
+    return render_template('404.html')
 
 
 @main.route('/')
