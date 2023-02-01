8bff9d409852efbd1c4d9e1666a25fb01dcc60f4
Elston D'Souza <elstonndsouza@gmail.com>
2023-02-01 22:48:10 +0000

    Update filepath

--- pipeline/downloads/download_omim.sh
+++ pipeline/downloads/download_omim.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
 # Downloads the omim map. 
+cd $(dirname "$0")
 
+cd ../../
 
 wget "https://www.omim.org/static/omim/data/mim2gene.txt" 
-mv mim2gene.txt ../../data/pipeline
+mv mim2gene.txt data/pipeline
