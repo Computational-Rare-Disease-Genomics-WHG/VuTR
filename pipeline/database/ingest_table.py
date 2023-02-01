025b858cf7878b5db75ba29df433606ac5d72edf
Elston D'Souza <elstonndsouza@gmail.com>
2023-02-01 22:45:42 +0000

    Create changes on the ingestion engine

--- pipeline/database/ingest_table.py
+++ pipeline/database/ingest_table.py
@@ -3,7 +3,16 @@ Takes a table and ingests it into the sqlite3 database
 
 Usage : python3 ingest_table.py --db_name db.sqlite --all --verbose --overwrite
 
-TODO : Dtype specification issue doesn't work
+TODO : Add indexing to this program for example.
+Especially for particularly large tables.
+
+CREATE INDEX idx_genome_to_transcript_coordinates ON
+ genome_to_transcript_coordinates(ensembl_transcript_id);
+CREATE INDEX idx_orf ON orf_features(ensembl_transcript_id);
+CREATE INDEX idx_mane_transcript_features ON
+    mane_transcript_features(ensembl_transcript_id);
+
+NOTE : Dtype specification issue doesn't work
     either through sqlcol or by manual specification in model.py
 """
 from pathlib import Path
@@ -31,16 +40,16 @@ def sqlcol(dfparam):
 
     dtypedict = {}
     for i, j in zip(dfparam.columns, dfparam.dtypes):
-        if 'object' in str(j):
+        if "object" in str(j):
             dtypedict.update({i: sqlalchemy.types.NVARCHAR()})
 
-        if 'datetime' in str(j):
+        if "datetime" in str(j):
             dtypedict.update({i: sqlalchemy.types.DateTime()})
 
-        if 'float' in str(j):
+        if "float" in str(j):
             dtypedict.update({i: sqlalchemy.types.Float(precision=3, asdecimal=True)})
 
-        if 'int' in str(j):
+        if "int" in str(j):
             dtypedict.update({i: sqlalchemy.types.INT()})
 
     return dtypedict
@@ -53,7 +62,7 @@ def strip_version_identifiers(df, id_names):
     """
 
     df.loc[:, id_names] = df.loc[:, id_names].replace(
-        to_replace='\.\d+$', value='', regex=True  # pylint: disable=W1401 # noqa: W605
+        to_replace="\.\d+$", value="", regex=True  # pylint: disable=W1401 # noqa: W605
     )
     return df
 
@@ -62,7 +71,7 @@ def main(args):
     """Entry point"""
     # check db name
     if os.path.isfile(args.db_name) and not args.overwrite:
-        print(f'Database already exists at {args.db_name}')
+        print(f"Database already exists at {args.db_name}")
         sys.exit(0)
 
     # Create connection and cursor with the database
@@ -78,60 +87,60 @@ def main(args):
     for tbl in tbl_list:
 
         # Read the sql table
-        print(f'Reading table {tbl} into pandas')
+        print(f"Reading table {tbl} into pandas")
         df = pd.read_csv(
-            SCRIPT_PATH / ('../../data/pipeline/' + tbl_models[tbl]['location']),
-            sep=tbl_models[tbl]['separator'],
+            SCRIPT_PATH / ("../../data/pipeline/" + tbl_models[tbl]["location"]),
+            sep=tbl_models[tbl]["separator"],
         )
 
         # Select columns specified
-        df = df.loc[:, tbl_models[tbl]['col_mappings'].keys()]
+        df = df.loc[:, tbl_models[tbl]["col_mappings"].keys()]
         # Replace column names with the col_mappings
-        df = df.rename(columns=tbl_models[tbl]['col_mappings'])
+        df = df.rename(columns=tbl_models[tbl]["col_mappings"])
 
         # Strip ensembl_identifiers for the columns
-        if tbl_models[tbl]['remove_ensembl_id_version_numbers']:
-            df = strip_version_identifiers(df, tbl_models[tbl]['ensembl_ids'])
+        if tbl_models[tbl]["remove_ensembl_id_version_numbers"]:
+            df = strip_version_identifiers(df, tbl_models[tbl]["ensembl_ids"])
 
         # Write to SQLtables
-        print(f'Writing to SQLite DB {args.db_name}')
-        df.to_sql(tbl, conn, if_exists='fail', index=False, chunksize=1000)
-        print(f'Ingestion for table {tbl} complete')
+        print(f"Writing to SQLite DB {args.db_name}")
+        df.to_sql(tbl, conn, if_exists="fail", index=False, chunksize=1000)
+        print(f"Ingestion for table {tbl} complete")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Ingresses all variant data into a sqlite3 database'
+        description="Ingresses all variant data into a sqlite3 database"
     )
     parser.add_argument(
-        '--db_name',
+        "--db_name",
         type=str,
-        help='Output sqlite3 file name and location',
+        help="Output sqlite3 file name and location",
     )
     parser.add_argument(
-        '--single',
-        action='store_true',
-        help='Single table',
+        "--single",
+        action="store_true",
+        help="Single table",
     )
     parser.add_argument(
-        '--all',
-        action='store_true',
-        help='All tables',
+        "--all",
+        action="store_true",
+        help="All tables",
     )
     parser.add_argument(
-        '--tbl_name',
-        required='single' in sys.argv,
+        "--tbl_name",
+        required="single" in sys.argv,
         type=str,
-        help='The name of the table to be ingested',
+        help="The name of the table to be ingested",
     )
     parser.add_argument(
-        '--overwrite',
-        action='store_true',
-        help='Overwrite existing database if exists',
+        "--overwrite",
+        action="store_true",
+        help="Overwrite existing database if exists",
     )
     parser.add_argument(
-        '--verbose',
-        action='store_true',
-        help='Verbose outputs',
+        "--verbose",
+        action="store_true",
+        help="Verbose outputs",
     )
     main(args=parser.parse_args())
