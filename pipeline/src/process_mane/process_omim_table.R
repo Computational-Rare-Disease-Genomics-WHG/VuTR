1a98a267254caa18b656982afc170df80049749d
Elston D'Souza <elstonndsouza@gmail.com>
2023-03-03 16:01:01 +0000

    Create process omim table

--- pipeline/src/process_mane/process_omim_table.R
+++ pipeline/src/process_mane/process_omim_table.R
@@ -3,8 +3,36 @@
 library(data.table)
 library(magrittr)
 
-b <- fread("mim2gene.txt")
+
+# Pass cmd line args
+parser <- OptionParser()
+
+# define options
+options_list <- list(
+  make_option(
+    c("-i", "--input"),
+    dest = "input",
+    type = "character",
+    help = "Path to the omim downloaded file from OMIM"
+  ),
+  make_option(
+    c("-o", "--output"),
+    dest = "output",
+    type = "character",
+    help = "Path to the output processed file"
+  )
+)
+parser <- add_options(parser, options_list)
+
+input_file_path <- options$input
+output_file_path <- options$output
+
+b <- fread(input_file_path)
+
 names(b) <- c("omim_entry", "type", "entrez", "hgnc", "ensembl_gene_id")
 b %<>% .[type == "gene"]
 b %<>% .[!(is.na(ensembl_gene_id)|ensembl_gene_id == "")]
-fwrite(b, "processed_omim.txt", sep = "\t")
+
+
+
+fwrite(b, output_file_path, sep = "\t")
