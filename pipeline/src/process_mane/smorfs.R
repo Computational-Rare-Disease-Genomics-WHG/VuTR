60ae977a273424a57a37f3301239d3f2fecae7a7
Elston Dsouza <elstonndsouza@gmail.com>
2024-02-22 16:24:04 +0000

    Fix filtering and add translational efficiency / kozak

--- pipeline/src/process_mane/smorfs.R
+++ pipeline/src/process_mane/smorfs.R
@@ -10,6 +10,46 @@ library("stringr")
 library("optparse")
 library("logger")
 
+
+#' Returns the TIS -6,+4
+#' @param pos
+#' @param cdna_seq
+#' @returns context : substring containing context TIS (pos-6,pos+4)
+get_context <- function(cdna_seq, pos) {
+    context <- substr(cdna_seq, pos - 6, pos + 4)
+    return(if (nchar(context) == 11 & !is.na(context)) context else NA)
+}
+#' Get the kozak context
+#' @param seq the transcript sequence
+#' @param start_site the location of the a in the start codon
+#' @return the 7 base kozak context, "-" if doesn't apply
+get_kozak_context <- function(seq, start_site) {
+    context <- substr(seq, start_site - 3, start_site + 3)
+    return(if (nchar(context) == 7) context else "-")
+}
+
+#' Get the kozak strength
+#' @param kozak_context, the kozak context of the sequence
+#' @return the strength of the kozak as per rules ("Strong", "Moderate", "Weak")
+get_kozak_consensus_strength <- function(kozak_context) {
+    if (nchar(kozak_context) != 7) {
+        return("None")
+    }
+    # Get the first and last bases that determine the kozak strength
+    fb <- substr(kozak_context, 1, 1)
+    lb <- substr(kozak_context, 7, 7)
+
+    # Check cases for the Kozak Strength
+    if ((fb == "a" || fb == "g") && lb == "g") {
+        return("Strong")
+    } else if ((fb == "a" || fb == "g") || lb == "g") {
+        return("Moderate")
+    } else {
+        return("Weak")
+    }
+}
+
+
 option_list <- list(
   make_option(
     c("-m", "--mane-file"),
@@ -18,6 +58,22 @@ option_list <- list(
     help = "Mane GFF file (as TSV)",
     metavar = "character"
   ),
+  make_option(
+    c("-r", "--rna-file"),
+    dest = "rna_file",
+    type = "character",
+    help = "File with MANE RNA sequences",
+    metavar = "character"
+  ),
+
+  make_option(
+    c("-e", "--translational-efficiency"),
+    dest = "te_file",
+    type = "character",
+    help = "File with translational efficiency",
+    metavar = "character"
+  ),
+
   make_option(
     c("-s", "--smorf"),
     type = "character",
@@ -46,11 +102,15 @@ mane_file <- opt$mane_file
 smorf_file <- opt$smorf
 genome_transcript_fn <- opt$g2tcoord
 output_file_fn <- opt$output_file
+te_file <- opt$te_file
+rna_file <- opt$rna_file
 
 log_info("Reading files")
 smorfs <- fread(smorf_file, sep="\t")
 mane <- fread(mane_file, sep="\t")
 tmap <- fread(genome_transcript_fn, sep="\t")
+te <- fread(te_file, sep="\t")
+rna <- fread(rna_file, sep="\t")
 
 # Renaming headers
 names(smorfs) <- c(
@@ -88,43 +148,44 @@ names(smorfs) <- c(
 ) # nolint
 
 all_chrs <- paste0("chr", c(1:22, "X", "Y"))
-
-# Filter to necessary chromosomes
-smorfs %<>% .[chr %in% all_chrs]
-
 # Create a unique "id" for each smorf
 smorfs[, id := .I]
+mane[, stable_transcript_id := substring(transcript_id, 1, 15)]
 setkey(smorfs, id)
-
-log_info("Filtering")
-# Filter down to five prime UTRs
-mane %<>% .[type == "five_prime_UTR"]
-mane %<>% .[, .(seqid, start, end, strand, transcript_id)]
-setkey(mane, start, end)
 
 # Convert smorf 0-based to 1-based coordinates bedFile
 # to match the mane coordinates
 # which are 1-based
-
 smorfs[, start :=  start + 1]
 
-# Find all smorfs that lie within the mane coordinates
-output <- smorfs[, {
-  mane[
-    seqid == .SD$chr &
-    strand == .SD$strand &
-    start <= .SD$start  &
-    .SD$end <= end][, .(transcript_id)]
-}, by = id]
 
+log_info("Filtering")
 
-setkey(output, id)
+# Filter down to five prime UTRs
+mane %<>% .[type == "exon"]
+mane %<>% .[, .(transcript_id, stable_transcript_id)]
+mane %<>% unique()
+
+# Filter to necessary chromosomes
+# Filter smorfs to those that are mapped to MANE coordinates
+# and are uORFs or uoORFs
+smorfs %<>% .[chr %in% all_chrs &
+  tx_id %in% mane$stable_transcript_id & 
+  (type == "uORF" | type == "uoORF")
+]
+
+# Merge with MANE to get transcript_id
+setkey(mane, stable_transcript_id)
+setkey(smorfs, tx_id)
+smorfs <- mane[smorfs]
 
 # Rename a few columns
 setnames(smorfs, "start", "genome_start")
 setnames(smorfs, "end", "genome_end")
 
-annotated_output <- smorfs[output]
+annotated_output <- smorfs
+
+log_info("Annotating transcript coordinates")
 
 # Convert IDs to tpos using transcript id and tpos
 setkey(tmap, transcript_id, gpos)
@@ -135,7 +196,8 @@ annotated_output[, transcript_start :=
   tmap[.(transcript_id = annotated_output$transcript_id,
   gpos = annotated_output$genome_start), .(tpos)]
 ]
-
+setkey(tmap, transcript_id, gpos)
+setkey(annotated_output, transcript_id, genome_end)
 annotated_output[, transcript_end :=
   tmap[.(transcript_id = annotated_output$transcript_id,
   gpos = annotated_output$genome_end), .(tpos)]
@@ -146,6 +208,36 @@ annotated_output[strand == "-", `:=` (
   transcript_start = transcript_end,
   transcript_end = transcript_start
 )]
+
+# # Add translational efficiency
+# log_info("Adding translational efficiency")
+rna %<>% .[, .(transcript_id, seq)]
+setkey(rna, transcript_id)
+setkey(annotated_output, transcript_id)
+# Merge cdna_seq with annotated_output
+annotated_output <- rna[annotated_output]
+annotated_output[start_codon == "ATG", context := get_context(seq, transcript_start), by = id]
+annotated_output[start_codon == "ATG", kozak_context := get_kozak_context(seq, transcript_start), by = id]
+annotated_output[start_codon == "ATG", kozak_consensus_strength := get_kozak_consensus_strength(kozak_context), by = id]
+
+# # Add translational efficiency
+te %<>% .[, .(context, efficiency)]
+setkey(te, context)
+setkey(annotated_output, context)
+annotated_output <- te[annotated_output]
+
+# # Remove cdna_seq
+annotated_output[, seq := NULL]
+
+# # For non-ATG start codons, set context and efficiency to NA
+annotated_output[
+  start_codon != "ATG",
+  `:=` (
+    context = NA,
+    efficiency = NA
+  )
+]
+
 
 log_info("Writing output")
 fwrite(
